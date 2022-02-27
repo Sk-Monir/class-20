e3e066f1beee9bf089d77e3325638a205648c968
Sk-Monir <96935864+Sk-Monir@users.noreply.github.com>
2022-02-27 10:59:40 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.banner_slider').slick({
+        autoplay: true,
+        prevArrow:'<i class="fa-solid fa-chevron-left banner_slider_icon"></i>',
+        nextArrow:'<i class="fa-solid fa-chevron-right banner_slider_icon"></i>',
+        dots:true,
+        dotsClass:'banner_slider_dots'
+    });
+});
